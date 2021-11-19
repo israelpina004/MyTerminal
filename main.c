c4915c2a68cee2ca940fdb538a8807956b1d8d5c
Aaron Visser <avisser20@stuy.edu>
2021-11-19 10:46:59 -0500

    added to main

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <string.h>
+
+int main() {
+char line[50];
+
+printf("Enter commands: \n");
+fgets(line, sizeof(line), stdin);
+int newline = strcspn(line, "\n");
+line[newline] = '\0';
+
+printf("\n");
+  char** args = parse_args(line);
+execvp(args[0], args);
+return 0;
+}
