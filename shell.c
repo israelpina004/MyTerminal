a695a30b4afb5b86afa29bceca32758db61a9c82
israelpina004 <90664097+israelpina004@users.noreply.github.com>
2021-11-19 11:01:34 -0500

    Update shell.c

--- shell.c
+++ shell.c
@@ -3,3 +3,28 @@
 #include <unistd.h>
 #include <signal.h>
 #include <sys/wait.h>
+
+char** parse_args(char* line) {
+	char** arr_args = calloc(5, sizeof(char *));
+	char* token;
+
+	int i;
+	for(i = 0; i < sizeof(line); i++) {
+		token = strsep(&line, " ");
+		arr_args[i] = token;
+	}
+	return arr_args;
+}
+
+void setup() {
+  char* user = getenv(USER);
+  
+  long size;
+  char* buff;
+  
+  size = pathconf(".", _PC_PATH_MAX);
+  buff = (char *)malloc((size_t)size))
+  if(buff != NULL) {
+    ptr = getcwd(buf, (size_t)size);
+  }
+}
