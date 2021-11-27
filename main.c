a0e1a9238b769bb5e9c7f42f8e56ba3ca6e3f1be
israelpina004 <90664097+israelpina004@users.noreply.github.com>
2021-11-27 01:02:50 -0500

    Update main.c

--- main.c
+++ main.c
@@ -17,7 +17,19 @@ int main() {
     int newline = strcspn(line, "\n");
     line[newline] = '\0';
 
-    char** args = parse_args(line);
+    //Supposed to parse the line from stdin by semicolons if there are any and
+    //then parse those arrays by spaces. Each sub-array is then supposed to be
+    //execvp'd. Not working as expected, but the program does not pass an error
+    //when a semicolon is present in the line, it just ignores the substrings
+    //that follow it.
+    char** args = parse_semis(line);
+    for(int i = 0; i < count_semis(line); i++) {
+      char** commands = parse_args(args[i]);
+      otherCommands(args);
+      runCommand(args);
+    }
+
+    args = parse_args(line);
     otherCommands(args);
     runCommand(args);
   }
