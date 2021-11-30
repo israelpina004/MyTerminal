93c5abaa32cc4c543fc640489aba19b0ba8b479b
Aaron Visser <avisser20@stuy.edu>
2021-11-29 21:43:34 -0500

    changes

---
+++ redirections.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+#include <string.h>
+#include "shell.h"
+#include "redirections.h"
+#include <sys/wait.h>
+
+void inputRedirection(char *line){
+  char * file = strsep(&line, "<");
+  int new_input = open(file, O_RDONLY, 0644);
+  int copy_of_input = dup(STDIN_FILENO);
+  dup2(new_input,STDIN_FILENO);
+  operate();
+  dup2(copy_of_input,STDIN_FILENO);
+}
+
+void outputRedirection(char *line){
+  char * path = strsep(&line, ">");
+  line ++;
+  int new_output = open(line, O_WRONLY|O_CREAT, 0644);
+  int copyof_output = dup(STDOUT_FILENO);
+  dup2(new_output,STDOUT_FILENO);
+  char ** args = parse_args(path);
+  runCommand(args);
+  dup2(copyof_output,STDOUT_FILENO);
+}
+
+void appendRedirection(char * line){
+  char * path = strsep(&line, ">>");
+  line ++;
+  printf("hi");
+  int new_output = open(line, O_APPEND|O_CREAT, 0644);
+  int old_output = dup(STDOUT_FILENO);
+  dup2(new_output,STDOUT_FILENO);
+  char ** args = parse_args(path);
+  runCommand(args);
+  dup2(old_output,STDOUT_FILENO);
+}
+void piping(char *line){
+  char** pipe_args = calloc(10, sizeof(char *));
+  char* token;
+
+  int i;
+  for(i = 0; i < sizeof(line); i++) {
+    token = strsep(&line, "|");
+    pipe_args[i] = token;
+  }
+
+  FILE *fd1 = popen(pipe_args[0], "r");
+  FILE *fd2 = popen(pipe_args[1], "w");
+  if(fd1 == NULL || fd2 == NULL) {
+    printf("Error piping.\n");
+  }
+
+  char input[500];
+  while(fgets(input, sizeof(input), fd1)) {
+    fputs(input, fd2);
+  }
+
+  pclose(fd1);
+  pclose(fd2);
+  free(pipe_args);
+}
---
+++ redirections.h
@@ -0,0 +1,4 @@
+void inputRedirection(char * line);
+void outputRedirection(char * line);
+void appendRedirection(char * line);
+void piping(char * line);
